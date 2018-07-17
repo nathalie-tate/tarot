933882324ce66d923d4253ef1de3659ade55cd67
Nathalie Tate <nathalie.tate00@gmail.com>
2018-07-16 22:18:05 -0400

    tarot.pl

---
+++ perl/tarot.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Math::Cartesian::Product;
+use List::Util qw/shuffle/;
+
+my $major = $ARGV[0];
+my $minor = $ARGV[1]; my $spread = $ARGV[2];
+
+open MajorFH, "<", $major or die;
+open MinorFH, "<", $minor or die;
+open SpreadFH, "<", $spread or die;
+
+my @major = <MajorFH>;
+my @minor = <MinorFH>; 
+my @spread = <SpreadFH>;
+
+my @suits;
+
+for(@minor)
+{
+  chomp;
+  unshift @suits,($1) if $_ =~ /#(.+)/ ;
+};
+
+shift @minor for @suits;
+chomp for @major;
+chomp for @spread;
+
+my @deck;
+
+cartesian{push @deck, "$_[0] of $_[1]" } [@minor], [@suits];
+
+@deck = (@deck, @major);
+@deck = shuffle(@deck);
+
+printRow($_) for @spread;
+
+sub printRow
+{
+  my $row = shift;
+  my @cards;
+  my @slots = $row =~ /../g;
+
+  my $count = () = $row =~ /(\d\d)/g;
+
+  for (0..$count)
+  {
+    push @cards, pop @deck;
+  }
+
+  for my $i (0..16)
+  {
+    my $cardNum = @cards;
+    for my $slot(0..@slots)
+    {
+      if($slots[$slot] and $slots[$slot] =~ /\d\d/)
+      {
+        my $tmp = $cards[--$cardNum];
+        my $len = length $tmp;
+        if($len%2)
+        {
+          $tmp.=" ";
+        }
+
+        if ($i == 0 || $i == 16)
+        {
+          print "+" . ("-"x26) . "+";
+        }
+        elsif( $i < 4 || $i > 4)
+        {
+          print "|". (" "x26) . "|";
+        }
+        elsif($i == 4)
+        {
+          print "|" .(" "x((26 - $len)/2)). $tmp .(" "x((26- $len)/2)). "|" if $cardNum;
+        }
+        else
+        {
+          die;
+        }
+      }
+      else
+      {
+        print " "x28;
+      }
+    }
+    print "\n"; 
+  }
+}
